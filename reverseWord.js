7323046edf9ba19970dea796b25293b308faa180
Robin Sanssi <robin.sanssi@icloud.com>
2026-08-26 13:43:04 +0200

    First commit incl koala

---
+++ reverseWord.js
@@ -0,0 +1,40 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Skriv något: ", (word) => {
+  console.log("Hej min vän! Du skrev: " + word);
+  const revereseWord = word.split("").reverse().join("");
+  console.log("Baklänges så blir det: " + revereseWord);
+
+  rl.question("Vill du se const, jag menar konst? (ja/nej): ", (answer) => {
+    if (answer.toLowerCase() === "ja") {
+      console.log(`
+
+                             |       :     . |
+             | '  :      '   |
+             |  .  |   '  |  |
+   .--._ _...:.._ _.--. ,  ' |
+  (  ,  \`        \`  ,  )   . |
+   '-/              \\-'  |   |
+     |  o   /\\   o  |       :|
+     \\     _\\/_     / :  '   |
+     /'._   ^^   _.;___      |
+   /\`    \`""""""\`      \`\\=   |
+ /\`                     /=  .|
+;             '--,-----'=    |
+|                 \`\\  |    . |
+\\                   \\___ :   |
+/'.                     \`\\=  |
+\\_/\`--......_            /=  |
+            |\`-.        /= : |
+            | : \`-.__ /\` .   |
+      `);
+    }
+
+    rl.close();
+  });
+});
